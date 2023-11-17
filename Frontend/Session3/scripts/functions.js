c63295e690cda22c1064423ebd6b0aa388ad450b
mareo1997 <mareo1997@gmail.com>
2023-11-17 13:18:59 -0500

    Session 4
    Tried to fix validation fields

--- Frontend/Session3/scripts/functions.js
+++ Frontend/Session3/scripts/functions.js
@@ -27,16 +27,56 @@ function signup(){
 
 function valid(){
 
-    able = true
-    
+    able = false
+
+    let name = document.getElementById('name').value;
+    const regexName = new RegExp('^[A-Za-z \-]*$');
+
     let repassword = document.getElementById('repassword').value;
 	let password = document.getElementById('password').value;
+    const regexPassword = "(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W){8}"
 
-    if(repassword.length >= 8 && password.length >= 8){
-        able = false
+	let phone = document.getElementById('phone').value;
+    const regexPhone = "[0-9]{3}-[0-9]{3}-[0-9]{4}"
+    // const regexPhone = "[0-9]{3}"
+
+    let email = document.getElementById('email').value;
+    const regexEmali = new RegExp('^.*@.*$');
+
+
+    // if(name.length == 0 || email.length == 0 || phone.length == 0){
+    //     able = true;
+    // }
+
+    if(!regexPhone.test(phone)){
+        alert("phone")
+        able = true;
+    }
+    if(!regexPassword.test(repassword)){
+        alert("repassword")
+        able = true;
+    }
+    if(!regexPassword.test(password)){
+        alert("password")
+        able = true;
+    }
+    // if(repassword.length < 8 || password.length < 8){
+    //     // alert()
+    //     able = true
+    // }
+    if(repassword != password){
+        // alert()
+        able = true
     }
 
-
+    if(!regexName.test(name)){
+        alert("name")
+        able = true;
+    }
+    if(!regexEmali.test(email)){
+        alert("@")
+        able = true;
+    }
     document.getElementById("login").disabled = able;
 
 }
